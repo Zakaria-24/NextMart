d948a650eb03620e3b79139e08bfa8c0936c3387
Zakaria <zakariajoarder.bd@gmail.com>
2025-03-11 04:13:12 +0600

    D

--- src/app/modules/coupon/coupon.routes.ts
+++ src/app/modules/coupon/coupon.routes.ts
@@ -1,34 +1,37 @@
-import { Router } from 'express';
-import auth from '../../middleware/auth';
-import { UserRole } from '../user/user.interface';
-import { couponController } from './coupon.controller';
-import validateRequest from '../../middleware/validateRequest';
-import { updateCouponValidationSchema } from './coupon.validation';
+import { Router } from "express";
+import auth from "../../middleware/auth";
+import { UserRole } from "../user/user.interface";
+import { couponController } from "./coupon.controller";
+import validateRequest from "../../middleware/validateRequest";
+import { updateCouponValidationSchema } from "./coupon.validation";
 
 const router = Router();
 
 // Define routes
-router.post('/', auth(UserRole.ADMIN), couponController.createCoupon);
+// router.post('/', auth(UserRole.ADMIN), couponController.createCoupon);
 
-router.get('/', auth(UserRole.ADMIN), couponController.getAllCoupon);
+// router.get('/', auth(UserRole.ADMIN), couponController.getAllCoupon);
+router.post("/", auth(UserRole.USER), couponController.createCoupon);
+
+router.get("/", auth(UserRole.USER), couponController.getAllCoupon);
 
 router.patch(
-   '/:couponCode/update-coupon',
-   validateRequest(updateCouponValidationSchema),
-   auth(UserRole.ADMIN),
-   couponController.updateCoupon
+  "/:couponCode/update-coupon",
+  validateRequest(updateCouponValidationSchema),
+  auth(UserRole.ADMIN),
+  couponController.updateCoupon
 );
 
 router.post(
-   '/:couponCode',
-   auth(UserRole.ADMIN, UserRole.USER), // Ensure only authorized users can fetch the coupon
-   couponController.getCouponByCode
+  "/:couponCode",
+  auth(UserRole.ADMIN, UserRole.USER), // Ensure only authorized users can fetch the coupon
+  couponController.getCouponByCode
 );
 
 router.delete(
-   '/:couponId',
-   auth(UserRole.ADMIN),
-   couponController.deleteCoupon
+  "/:couponId",
+  auth(UserRole.ADMIN),
+  couponController.deleteCoupon
 );
 
 export const CouponRoutes = router;
